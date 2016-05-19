21fef32070c33a42f192a267c2e8fa5e1cd9a84c
Alexander Baranov <sashab1@yandex-team.ru>
2016-05-19 16:32:04 +0300

    Do not import XRootD/WebDAV by default

--- skygridbackends/__init__.py
+++ skygridbackends/__init__.py
@@ -2,7 +2,7 @@ import base
 
 from git import GitBackend
 from local import LocalBackend
-from webdav import WebDAVBackend
-from xrootd import XrootdBackend
+#from webdav import WebDAVBackend
+#from xrootd import XrootdBackend
 
 from multi import MultiBackend
