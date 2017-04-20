7ac8daf7b0d76f7dbac3200736f75e44ee950584
Alexander Baranov <sashab1@yandex-team.ru>
2017-04-20 12:35:56 +0300

    Use separate package for backends

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='skygrid-docker-worker',
-    version='0.4.18',
+    version='0.4.19',
     url='https://github.com/skygrid/docker-worker',
     author='Alexander Baranov',
     author_email='sashab1@yandex-team.ru',
@@ -16,12 +16,9 @@ setup(
         "six==1.9.0",
         "websocket-client>=0.26.0",
         "wsgiref==0.1.2",
-        "easywebdav-dcache==1.2.4", # fixed easywebdav, see https://github.com/amnong/easywebdav/pull/37
         "raven",
-        "gitpython",
-        "sh>=1.11",
+        "hep-data-backends",
     ],
-
     scripts = [
         'scripts/test-descriptor'
     ]
--- skygridbackends/base.py
+++
@@ -1,9 +0,0 @@
-class BackendBase(object):
-    def copy_from_backend(self, src_path, dst_path):
-        raise NotImplementedError()
-
-    def copy_to_backend(self, src_path, dst_path):
-        raise NotImplementedError()
-
-    def list_uploaded(self, path):
-        raise NotImplementedError()
