954a9c05405a0200be03f74069aeb73e8a49cc70
Unknown <igor.musinov@phystech.edu>
2018-12-28 19:27:51 +0300

    up

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='skygrid-docker-worker',
-    version='0.7.0',
+    version='0.8.0',
     url='https://github.com/wonderlandcompute/docker-worker',
     author='Alexander Baranov && Musinov Igor',
     author_email='sashab1@yandex-team.ru',
